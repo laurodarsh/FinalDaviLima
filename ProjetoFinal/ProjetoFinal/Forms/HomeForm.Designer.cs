82e71f93d85b275f9f780f0b4549e43a21859abd
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-05-09 15:50:38 -0300

    HomeForm criado

---
+++ ProjetoFinal/ProjetoFinal/Forms/HomeForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ProjetoFinal
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblPerfil = new System.Windows.Forms.Label();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.pbxCategory = new System.Windows.Forms.PictureBox();
+            this.pbxUser = new System.Windows.Forms.PictureBox();
+            this.pbxProfile = new System.Windows.Forms.PictureBox();
+            this.pbxLog = new System.Windows.Forms.PictureBox();
+            this.pbxProduct = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCategory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxProfile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLog)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.AutoSize = true;
+            this.lblProduto.Location = new System.Drawing.Point(70, 110);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(44, 13);
+            this.lblProduto.TabIndex = 5;
+            this.lblProduto.Text = "Produto";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(271, 110);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblCategoria.TabIndex = 6;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(456, 110);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 7;
+            this.lblUsuario.Text = "Usuário";
+            // 
+            // lblPerfil
+            // 
+            this.lblPerfil.AutoSize = true;
+            this.lblPerfil.Location = new System.Drawing.Point(182, 248);
+            this.lblPerfil.Name = "lblPerfil";
+            this.lblPerfil.Size = new System.Drawing.Size(30, 13);
+            this.lblPerfil.TabIndex = 8;
+            this.lblPerfil.Text = "Perfil";
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.Location = new System.Drawing.Point(382, 248);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(25, 13);
+            this.lblLog.TabIndex = 9;
+            this.lblLog.Text = "Log";
+            // 
+            // pbxCategory
+            // 
+            this.pbxCategory.BackgroundImage = global::ProjetoFinal.Properties.Resources.category;
+            this.pbxCategory.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxCategory.Location = new System.Drawing.Point(242, 44);
+            this.pbxCategory.Name = "pbxCategory";
+            this.pbxCategory.Size = new System.Drawing.Size(100, 50);
+            this.pbxCategory.TabIndex = 4;
+            this.pbxCategory.TabStop = false;
+            // 
+            // pbxUser
+            // 
+            this.pbxUser.BackgroundImage = global::ProjetoFinal.Properties.Resources.user;
+            this.pbxUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxUser.Location = new System.Drawing.Point(427, 44);
+            this.pbxUser.Name = "pbxUser";
+            this.pbxUser.Size = new System.Drawing.Size(100, 50);
+            this.pbxUser.TabIndex = 3;
+            this.pbxUser.TabStop = false;
+            // 
+            // pbxProfile
+            // 
+            this.pbxProfile.BackgroundImage = global::ProjetoFinal.Properties.Resources.user_profile;
+            this.pbxProfile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxProfile.Location = new System.Drawing.Point(148, 179);
+            this.pbxProfile.Name = "pbxProfile";
+            this.pbxProfile.Size = new System.Drawing.Size(100, 50);
+            this.pbxProfile.TabIndex = 2;
+            this.pbxProfile.TabStop = false;
+            // 
+            // pbxLog
+            // 
+            this.pbxLog.BackgroundImage = global::ProjetoFinal.Properties.Resources.log;
+            this.pbxLog.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxLog.Location = new System.Drawing.Point(343, 179);
+            this.pbxLog.Name = "pbxLog";
+            this.pbxLog.Size = new System.Drawing.Size(100, 50);
+            this.pbxLog.TabIndex = 1;
+            this.pbxLog.TabStop = false;
+            // 
+            // pbxProduct
+            // 
+            this.pbxProduct.BackgroundImage = global::ProjetoFinal.Properties.Resources.product;
+            this.pbxProduct.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxProduct.Location = new System.Drawing.Point(44, 44);
+            this.pbxProduct.Name = "pbxProduct";
+            this.pbxProduct.Size = new System.Drawing.Size(100, 50);
+            this.pbxProduct.TabIndex = 0;
+            this.pbxProduct.TabStop = false;
+            this.pbxProduct.Click += new System.EventHandler(this.pbxProduct_Click);
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 323);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.lblPerfil);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblProduto);
+            this.Controls.Add(this.pbxCategory);
+            this.Controls.Add(this.pbxUser);
+            this.Controls.Add(this.pbxProfile);
+            this.Controls.Add(this.pbxLog);
+            this.Controls.Add(this.pbxProduct);
+            this.Name = "HomeForm";
+            this.Text = "HomeForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCategory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxProfile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLog)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxProduct;
+        private System.Windows.Forms.PictureBox pbxLog;
+        private System.Windows.Forms.PictureBox pbxProfile;
+        private System.Windows.Forms.PictureBox pbxUser;
+        private System.Windows.Forms.PictureBox pbxCategory;
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblPerfil;
+        private System.Windows.Forms.Label lblLog;
+    }
+}
