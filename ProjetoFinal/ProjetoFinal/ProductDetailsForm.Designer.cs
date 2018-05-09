831d8d063ed840290d1c3067e5e3005f5ac5a31c
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-05-09 16:21:50 -0300

    Criando Details Form

---
+++ ProjetoFinal/ProjetoFinal/ProductDetailsForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ProjetoFinal
+{
+    partial class ProductDetailsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblPreço = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblAtivo = new System.Windows.Forms.Label();
+            this.cbxAtivo = new System.Windows.Forms.CheckBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.tbxNome = new System.Windows.Forms.TextBox();
+            this.tbxPreço = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(45, 50);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(57, 18);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome :";
+            // 
+            // lblPreço
+            // 
+            this.lblPreço.AutoSize = true;
+            this.lblPreço.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPreço.Location = new System.Drawing.Point(45, 93);
+            this.lblPreço.Name = "lblPreço";
+            this.lblPreço.Size = new System.Drawing.Size(58, 18);
+            this.lblPreço.TabIndex = 1;
+            this.lblPreço.Text = "Preço :";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.Location = new System.Drawing.Point(18, 131);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(85, 18);
+            this.lblCategoria.TabIndex = 2;
+            this.lblCategoria.Text = "Categoria :";
+            this.lblCategoria.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblAtivo
+            // 
+            this.lblAtivo.AutoSize = true;
+            this.lblAtivo.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAtivo.Location = new System.Drawing.Point(49, 173);
+            this.lblAtivo.Name = "lblAtivo";
+            this.lblAtivo.Size = new System.Drawing.Size(52, 18);
+            this.lblAtivo.TabIndex = 3;
+            this.lblAtivo.Text = "Ativo :";
+            // 
+            // cbxAtivo
+            // 
+            this.cbxAtivo.AutoSize = true;
+            this.cbxAtivo.Location = new System.Drawing.Point(128, 177);
+            this.cbxAtivo.Name = "cbxAtivo";
+            this.cbxAtivo.Size = new System.Drawing.Size(15, 14);
+            this.cbxAtivo.TabIndex = 4;
+            this.cbxAtivo.UseVisualStyleBackColor = true;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(128, 131);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(100, 21);
+            this.cmbCategoria.TabIndex = 5;
+            // 
+            // tbxNome
+            // 
+            this.tbxNome.Location = new System.Drawing.Point(128, 50);
+            this.tbxNome.Name = "tbxNome";
+            this.tbxNome.Size = new System.Drawing.Size(100, 20);
+            this.tbxNome.TabIndex = 6;
+            // 
+            // tbxPreço
+            // 
+            this.tbxPreço.Location = new System.Drawing.Point(128, 93);
+            this.tbxPreço.Name = "tbxPreço";
+            this.tbxPreço.Size = new System.Drawing.Size(100, 20);
+            this.tbxPreço.TabIndex = 7;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::ProjetoFinal.Properties.Resources.delete;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(411, 247);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(49, 34);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::ProjetoFinal.Properties.Resources.add;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(342, 248);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 33);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::ProjetoFinal.Properties.Resources.back;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 247);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 34);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ProductDetailsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 293);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbxPreço);
+            this.Controls.Add(this.tbxNome);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.cbxAtivo);
+            this.Controls.Add(this.lblAtivo);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblPreço);
+            this.Controls.Add(this.lblNome);
+            this.Name = "ProductDetailsForm";
+            this.Text = "ProductDetailsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblPreço;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblAtivo;
+        private System.Windows.Forms.CheckBox cbxAtivo;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.TextBox tbxNome;
+        private System.Windows.Forms.TextBox tbxPreço;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
